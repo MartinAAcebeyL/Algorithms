1c6a6429e05ecdef25702a32d9be6e101391bee4
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-08-16 22:34:14 -0400

    add Basic Union Find Class

---
+++ UnionFind/DynamicConnectivity/BasicUF.java
@@ -0,0 +1,39 @@
+package UnionFind.DynamicConnectivity;
+
+
+class BasicUF {
+    private int id[];
+
+    BasicUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i; // each object is in its own component
+        }
+    }
+
+    void union(int p, int q) {
+        // add connection between p and q
+        int pid = find(p);
+        int qid = find(q);
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid)
+                id[i] = qid;
+        }
+    }
+
+    boolean connected(int p, int q) {
+        // are p and q in the same component?
+        return id[p] == id[q];
+    }
+
+    int find(int p) {
+        // component identifier for p (0 to n-1)
+        return id[p];
+    }
+
+    int count() {
+        // number of components
+        return id.length;
+    }
+}
---
+++ UnionFind/DynamicConnectivity/Main.java
@@ -0,0 +1,21 @@
+package UnionFind.DynamicConnectivity;
+
+
+public class Main {
+    public static void main(String[] args) {
+        BasicUF uf = new BasicUF(10);
+        uf.union(4, 3);
+        uf.union(3, 8);
+        uf.union(6, 5);
+        uf.union(9, 4);
+        uf.union(2, 1);
+        System.out.println(uf.connected(0, 7));
+        System.out.println(uf.connected(8, 9));
+
+        uf.union(5, 0);
+        uf.union(7, 2);
+        uf.union(6, 1);
+        uf.union(1, 0);
+        System.out.println(uf.connected(0, 7));
+    }
+}
